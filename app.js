a339342d411f7896bc8e506f7d3c98f84b3569e2
shoobah <shoobah@gmail.com>
2013-11-30 03:25:23 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,68 @@
+/*global require, process*/
+/**
+ * Module dependencies.
+ */
+'use strict';
+
+var express = require('express');
+//var routes = require('./routes');
+//var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var data = require('./core/InsiktenData');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//app.get('/', routes.index);
+//app.get('/users', user.list);
+
+//data.getPage(210);
+
+app.get('/', function(req, res) {
+    res.render('index')
+});
+
+app.get('/page:id', function(req, res) {
+    var id = parseInt(req.params.id.replace(':', ''));
+    data.getPage(id, function(doc) {
+        res.send(doc);
+    });
+});
+
+
+    //    var mainBody = "";
+    //    var properties = doc.Properties;
+    //    if (properties) {
+    //        for (var i=0; i < properties.length; i++) {
+    //            if (properties[i].Name === 'MainBody') {
+    //                mainBody = properties[i].Value;
+    //            }
+    //        }
+    //        res.send(mainBody);
+    //    } else
+    //        res.send("MainBody not found!");
+    //});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ core/InsiktenData.js
@@ -0,0 +1,20 @@
+﻿/*global console, require*/
+var MongoClient = require('mongodb').MongoClient;
+
+exports.getPage = function(pageId, callback) {
+    MongoClient.connect('mongodb://127.0.0.1:27017/insikten', function(err, db) {
+
+        if (err) {
+            throw err;
+        }
+
+        var collection = db.collection('pages');
+
+        collection.findOne({ Id: pageId }, function(dataError, doc) {
+            if (dataError) {
+                throw dataError;
+            }
+            callback(doc);
+        });
+    });
+};
